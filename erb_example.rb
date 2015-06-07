96056076667cbc513ef54a61b0524235fa82827e
zvtibo <zvtibo2012@gmail.com>
2015-06-06 18:09:39 -0700

    using erb

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,7 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+z = 12
+y = {:history => "remote", :art => "mysterious", :math => "strict"}
+template = ERB.new "The value of z is: <%= z + x %>"
+puts template.result(binding)
